d89d5783d602907bf544bd83e760ff6b7ddb184b
jailsonpj <jailsoncolares@gmail.com>
2017-12-12 11:16:21 -0400

    feat: exemplos capitulo 3

---
+++ cap3/exemplo1.py
@@ -0,0 +1,16 @@
+def print_lyrics():
+	print("I'm a lumberjack, and I'm okay.")
+	print("I sleep all night and I work all day.")
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+#repeat_lyrics()
+
+def print_twice(bruce):
+	print(bruce)
+	print(bruce)
+
+print_twice('Spam')
+print_twice(42)
