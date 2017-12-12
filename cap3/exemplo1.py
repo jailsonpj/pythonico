85c608b3832a847e311928efc39a63f3583b1bdb
jailsonpj <jailsoncolares@gmail.com>
2017-12-12 11:56:57 -0400

    feat: exemplos capitulo 3

--- cap3/exemplo1.py
+++ cap3/exemplo1.py
@@ -12,5 +12,13 @@ def print_twice(bruce):
 	print(bruce)
 	print(bruce)
 
-print_twice('Spam')
-print_twice(42)
+def cat_twice(part1,part2):
+	cat = part1 + part2
+	print_twice(cat)
+
+#print_twice(42) 
+line1 = 'Bing tiddle '
+line2 = 'tiddle bang'
+
+cat_twice(line1,line2)
+
