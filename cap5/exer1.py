d548f28655048661c768ee28e84c055708170114
jailsonpj <jailsoncolares@gmail.com>
2017-12-14 14:59:12 -0400

    subindo alterações

--- cap5/exer1.py
+++ cap5/exer1.py
@@ -15,5 +15,15 @@ def fibo(n):
 	else:
 		return fibo(n-1) + fibo(n-2)
 
+def ack(m,n):
+	if m == 0:
+		return n+1
+	elif m > 0 and n == 0:
+		return ack(m-1,1)
+	else:
+		return ack(m-1,ack(m,n-1))
+
+
 print(fatorial(5))
 print(fibo(3))
+print(ack(3,4))
