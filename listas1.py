4bc26e57f18c0b7fda2c49bf0511ba11c7cd3a8b
Jailson Pereira <jailsoncolares@gmail.com>
2017-12-28 13:40:40 -0400

    novo exemplo do uso de listas
    novo exemplo do uso de listas  com os comandos pop ,remove e del.

---
+++ listas1.py
@@ -0,0 +1,20 @@
+
+t = ['a','b','c']
+x = t.pop(1)
+print(x)
+
+lista = ['c','d','e','f']
+del lista[1]
+print(lista)
+
+'''for i in range(len(lista)):
+	print(lista[i])
+'''
+
+lista1 = ['j','k','l','w']
+lista1.remove('k')
+print(lista1)
+
+
+del lista1[1:2]
+print(lista1)
